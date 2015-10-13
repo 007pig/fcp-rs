b676ebe0e4991eafe0e2a4d663f73b6526e55808
Xiaoyu Huang <xiaoyu.huang@btcchina.com>
2015-10-13 17:06:25 +0800

    Testing code is working

--- src/lib.rs
+++ src/lib.rs
@@ -1,3 +1,31 @@
+use std::io::prelude::*;
+use std::net::TcpStream;
+use std::io::BufReader;
+
+
 #[test]
 fn it_works() {
+    let mut stream = TcpStream::connect("127.0.0.1:9481").unwrap();
+
+    let _ = stream.write(b"ClientHello\nName=My Freenet Client\nExpectedVersion=2.0\nEndMessage\n");
+    println!("stream sent!");
+    
+
+    //let mut buf = String::new();
+
+    println!("reading!");
+
+    let mut reader = BufReader::new(stream);
+    
+    //let n = reader.read_line(&mut buf);
+
+    for line in reader.lines() {
+        let msg = line.unwrap();
+        
+        println!("buf: {:?}", &msg);
+
+        if msg == "EndMessage" {
+            break;
+        }
+    }
 }

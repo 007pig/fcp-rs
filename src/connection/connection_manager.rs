55baef0a45b5ea46d0d4914eec64cde281b4ca9a
Xiaoyu Huang <xiaoyu.huang@btcchina.com>
2015-10-19 22:00:54 +0800

    improving connection manager (not done)

--- src/connection/connection_manager.rs
+++ src/connection/connection_manager.rs
@@ -1,4 +1,7 @@
 use std::net::ToSocketAddrs;
+use std::collections::HashMap;
+use std::thread;
+use std::time::Duration;
 
 use super::Connection;
 
@@ -10,12 +13,30 @@ enum ConnectionStatus {
 }
 
 pub struct ConnectionManager {
-    connections: Vec<ConnectionStatus>,
+    connections: HashMap<&str, ConnectionStatus>,
 }
 
 impl ConnectionManager {
-    pub fn create_connection<A: ToSocketAddrs>(addr: A) {
-        let connection = Connection.connect(addr)
+    pub fn new() -> ConnectionManager {
+        ConnectionManager {
+            connections: HashMap::new(),
+        }
     }
     
+    pub fn create_connection<A: ToSocketAddrs>(&mut self, key: &str, addr: A) {
+        let connection = Connection.connect(addr);
+
+        while connection.is_err() {
+            // Sleep some time and try again
+            thread::sleep_ms(5000);
+            
+            connection = Connection.connect(attr);
+        }
+
+        self.connections.insert(key, connection);
+    }
+
+    pub fn get_connection(&self, key: &str) -> ConnectionStatus {
+        self.connections.get(key)
+    }
 }
